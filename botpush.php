14c8233e3d2591274fe931afcb6e265597cb2d07
onaja <39725851+onaja@users.noreply.github.com>
2018-10-23 18:00:51 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -137,6 +137,8 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         curl_close ($ch);
+    
+    
     }
    exit;
 ?>
